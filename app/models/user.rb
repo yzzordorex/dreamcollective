cc34825358a9aea551f078b26042911d33585de7
Alex Bisceglie <me@alexbisceglie.com>
2017-03-09 12:12:22 -0800

    set ip/last-login on user for each user session creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true
+
+  def update_session_attrs(remote_ip)
+    begin
+      update(last_login: DateTime.current, ip_address: remote_ip)
+      self.save
+    rescue
+      # log something
+    end
+  end
 end
