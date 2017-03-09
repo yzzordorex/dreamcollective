0051f6b81bf692fab536794fb7750194d74e966c
Alex Bisceglie <me@alexbisceglie.com>
2017-03-08 17:19:26 -0800

    tests for user controller actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
+  test "should get login" do
+    get login_path
+    assert_response :success
+  end
+  test "should get signup" do
+    get signup_path
     assert_response :success
   end
 end
