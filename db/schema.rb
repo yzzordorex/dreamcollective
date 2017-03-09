9f81b30d260dd595dc916b071fdd511067710b59
Alex Bisceglie <me@alexbisceglie.com>
2017-03-08 17:01:14 -0800

    flesh out user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309002537) do
+ActiveRecord::Schema.define(version: 20170309005813) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "username"
+    t.string   "real_name"
+    t.text     "profile",         limit: 65535
   end
 
 end
