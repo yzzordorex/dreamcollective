df8a749e5fdf14546eae84f14803b97aeb9a215c
Alex Bisceglie <me@alexbisceglie.com>
2017-03-08 16:47:02 -0800

    all the users/login base

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(allowed_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Thanks for signing up!'
+    else
+      render :new
+    end
+  end
+
+  private
+  def allowed_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'sessions/new'
+  resources :sessions
+  resources :users
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root :to => 'home#index'
 end
